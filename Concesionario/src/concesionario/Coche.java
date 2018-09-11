ef4dfa3ca53fbe1af0bfc88f4c4de49c83a34eb3
mati <mati@cicles>
2018-09-11 10:36:18 +0200

    commit number one

---
+++ Concesionario/src/concesionario/Coche.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package concesionario;
+
+/**
+ *
+ * @author mati
+ */
+public class Coche {
+    private String matricula;
+    private String marca;
+    private String modelo;
+    private String potencia;
+    private String color;
+    private Double precio;
+}
